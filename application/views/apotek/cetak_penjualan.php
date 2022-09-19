d1596209103bb6bce66f71da99becbfb9fa0ed1c
webprograming <alldii1956@gmail.com>
2022-09-19 09:37:02 +0700

    cetak faktur

--- application/views/apotek/cetak_penjualan.php
+++ application/views/apotek/cetak_penjualan.php
@@ -9,31 +9,85 @@
 		table, th, td {
 			border: 1px solid black;
 			border-collapse: collapse;
-			},
+		},
 
-			td{
-				text-align: center;
-			}
-		</style>
-	</head><body>
+		td{
+			/*	text-align: center;*/
+		}
+	</style>
+</head><body>
 
-		<table border="1">
-			<tr>
-				<td><p>APOTEK SUKARAJA</p></td>
-				<td width="400"><p style="text-align: center;">FAKTUR</p></td>
-			</tr>
+	<table border="0">
+		<tr>
+			<td><p>APOTEK SUKARAJA</p></td>
+			<td width="400"><p style="text-align: center;">FAKTUR</p></td>
+		</tr>
+
+	</table>
+	<br>
+
+	<table border="0">
+		<tr>
+			<td width="112">Faktur No</td>
+			<td width="200">: 036/09/2022</td>
+			<td width="">Kepada Yth :</td>
+		</tr>
+		<tr>
+			<td width="113">Tanggal</td>
+			<td width="300">: 19/09/2022</td>
+			<td width="">DR Supratman TR</td>
+		</tr>
+		<tr>
+			<td width="113"></td>
+			<td width="250"></td>
+			<td width=""><p>Stabat Langkat</p></td>
+		</tr>
+	</table>
+
+	<table border="0">
+		<tr>
+			<td>NB. : FAKT 1 (NEW DIATAB KOSONG BRG) </td>
 			
-		</table>
-		<br>
-
-		<table border="1">
-			<tr>
-				<td>Faktur No</td>
-				<td width="100">:</td>
-			</tr>
+		</tr>
+	</table>
+	<table style="text-align: center; margin-top:5px;">
+		<tr>
+			<th width="100">QTY</th>
+			<th width="200">Keterangan</th>
+			<th width="100">Harga @</th>
+			<th width="100">Total</th>
 			
-		</table>
+		</tr>
+		<tr>
+			<td>144 Btl</td>
+			<td>ZACOLDINE SYR</td>
+			<td>Rp 34343</td>
+			<td>Rp 3434343</td>
+		</tr>
+		<tr>
+			<th width="100">Diterima Oleh</th>
+			<th width="200">Hormat Kami</th>
+			<th width="100">Total</th>
+			<th width="100">Rp 343434343</th>
+			
+		</tr>
+		
+	</table>
 
-		<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-	</body></html>
+	<table border="0" style="margin-top: 24px;">
+		<tr>
+			<th width="100">
+				___________
+			</th>
+			<th width="200">
+				___________
+			</th>
+			<th width="100"></th>
+			<th width="100"></th>
+			
+		</tr>
+	</table>
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body></html>
