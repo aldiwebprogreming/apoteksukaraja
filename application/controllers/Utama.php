f4ff1cb8f0244d32206f2ee264399ccfe297807f
webprograming <alldii1956@gmail.com>
2022-10-31 13:13:46 +0700

    baru

--- application/controllers/Utama.php
+++ application/controllers/Utama.php
@@ -805,7 +805,7 @@
 				'nama_barang' => $get['nama_produk'],
 				'harga' => $get['harga_jual'],
 				'qty' => $qty,
-				'harga_total' => $total_harga,
+				'harga_total' => $total_hargas,
 				'total' => ''
 			];
 
@@ -894,9 +894,35 @@
 		}
 
 		function test(){
-			$a = '4.500' * 2;
-			echo $a;
+			
+			$api_key   = '2ad1c6ff045383d38f2ba7f13ad9d225f5794930'; // API KEY Anda
+			$id_device = '117'; // ID DEVICE yang di SCAN (Sebagai pengirim)
+			$url   = 'https://api.watsap.id/send-message'; // URL API
+			$no_hp = '083138184143'; // No.HP yang dikirim (No.HP Penerima)
+			$pesan = '😁 Halo Terimakasih 🙏'; // Pesan yang dikirim
 
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_HEADER, 0);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 0); // batas waktu response
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($curl, CURLOPT_POST, 1);
+
+			$data_post = [
+				'id_device' => $id_device,
+				'api-key' => $api_key,
+				'no_hp'   => $no_hp,
+				'pesan'   => $pesan
+			];
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data_post));
+			curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+			$response = curl_exec($curl);
+			curl_close($curl);
+			echo $response;
 		}
 
 
